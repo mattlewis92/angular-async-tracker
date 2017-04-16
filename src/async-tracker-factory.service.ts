4ed24189b04d1aa39c967c11545edcdeb683eed7
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-04-16 16:48:51 +0100

    feat: initial support for promises

--- src/async-tracker-factory.service.ts
+++ src/async-tracker-factory.service.ts
@@ -1,4 +1,35 @@
+const isActive: symbol = Symbol('isActive');
+const tracking: symbol = Symbol('tracking');
+
 export class AsyncTracker {
+
+  constructor() {
+    this[isActive] = false;
+    this[tracking] = [];
+  }
+
+  get active(): boolean {
+    return this[isActive];
+  }
+
+  add(promise: Promise<any>): void {
+    this[tracking].push(promise);
+    this.updateIsActive();
+    promise.then(() => {
+      this.removeFromTracking(promise);
+    }, () => {
+      this.removeFromTracking(promise);
+    });
+  }
+
+  private removeFromTracking(promise: Promise<any>): void {
+    this[tracking] = this[tracking].filter(iPromise => iPromise !== promise);
+    this.updateIsActive();
+  }
+
+  private updateIsActive(): void {
+    this[isActive] = this[tracking].length > 0;
+  }
 
 }
 
--- test/async-tracker-factory.service.spec.ts
+++ test/async-tracker-factory.service.spec.ts
@@ -1,8 +1,15 @@
-import {
-  TestBed
-} from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { expect } from 'chai';
 import { AsyncTrackerModule, AsyncTrackerFactory, AsyncTracker } from '../src';
+
+function createPromise(): {promise: Promise<any>, resolve: Function, reject: Function} {
+  let resolve: Function, reject: Function;
+  let promise: Promise<any> = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+  return {promise, resolve, reject};
+}
 
 describe('async-tracker-factory service', () => {
 
@@ -21,5 +28,27 @@ describe('async-tracker-factory service', () => {
     const tracker: AsyncTracker = trackerFactory.create();
     expect(tracker instanceof AsyncTracker).to.be.true;
   });
+
+  it('should track promises that resolve', async(() => {
+    const tracker: AsyncTracker = trackerFactory.create();
+    const {promise, resolve} = createPromise();
+    tracker.add(promise);
+    expect(tracker.active).to.be.true;
+    resolve();
+    setTimeout(() => {
+      expect(tracker.active).to.be.false;
+    });
+  }));
+
+  it('should track promises that reject', async(() => {
+    const tracker: AsyncTracker = trackerFactory.create();
+    const {promise, reject} = createPromise();
+    tracker.add(promise);
+    expect(tracker.active).to.be.true;
+    reject();
+    setTimeout(() => {
+      expect(tracker.active).to.be.false;
+    });
+  }));
 
 });
